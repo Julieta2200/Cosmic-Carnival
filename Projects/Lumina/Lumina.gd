8f4d5fdee94bdec790ac7aa98b93e6baecb4534d
Julieta2200 <julieta@megista.io>
2024-09-30 18:58:27 +0400

    lumina animation

--- Projects/Lumina/Lumina.gd
+++ Projects/Lumina/Lumina.gd
@@ -10,3 +10,14 @@ func move():
 	var direction = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = direction * speed
 	move_and_slide()
+	animation()
+
+func animation()-> void:
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.play("idle")
+		return
+	if (velocity).y < 0:
+		$AnimatedSprite2D.play("back_walk")
+	else:
+		$AnimatedSprite2D.play("walk")
+
