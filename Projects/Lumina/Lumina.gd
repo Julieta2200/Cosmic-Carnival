99e8c165cfc9343d4038fc8cfdc105105ccbc6c8
Julieta2200 <julieta@megista.io>
2024-09-30 19:03:11 +0400

    anim

--- Projects/Lumina/Lumina.gd
+++ Projects/Lumina/Lumina.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 
-
 @export var speed : int 
+var direction : Vector2
 
 func _physics_process(_delta):
 	move()
 	
 func move():
-	var direction = Input.get_vector("left", "right", "up", "down").normalized()
+	direction = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = direction * speed
 	move_and_slide()
 	animation()
 
 func animation()-> void:
-	if velocity == Vector2.ZERO:
+	if direction == Vector2.ZERO:
 		$AnimatedSprite2D.play("idle")
 		return
-	if (velocity).y < 0:
+	if (direction).y < 0:
 		$AnimatedSprite2D.play("back_walk")
 	else:
 		$AnimatedSprite2D.play("walk")
