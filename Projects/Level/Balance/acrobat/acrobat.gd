5cba60239b89fd6bc4b76ede90d20d582b1c5321
Julieta2200 <julieta@megista.io>
2024-10-10 20:29:56 +0400

    rotation_degrees

--- Projects/Level/Balance/acrobat/acrobat.gd
+++ Projects/Level/Balance/acrobat/acrobat.gd
@@ -4,29 +4,36 @@ class_name Acrobat extends Node2D
 @export var move_speed: int
 @export var move_multiplier: int
 @export var gravitation_speed: float
+var distance_traveled = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var total_rotation: float
-	
-	if rotation_degrees > 0:
-		total_rotation += gravitation_speed
+
+	if rotation_degrees >= 0 && rotation_degrees < 90:
+		if rotation_degrees < 30:
+			total_rotation += gravitation_speed
+		else:
+			total_rotation += 2 * gravitation_speed
+	elif rotation_degrees < 0 && rotation_degrees > -90:
+		if rotation_degrees > -30:
+			total_rotation -= gravitation_speed
+		else:
+			total_rotation -= 2 * gravitation_speed
 	else:
-		total_rotation -= gravitation_speed
-	
+		total_rotation -= 0
+
 	if Input.is_action_pressed("A"):
+		distance_traveled += 10
 		total_rotation *= move_multiplier
-		
+
 	if Input.is_action_pressed("left"):
 		total_rotation -= rotation_speed
 	elif Input.is_action_pressed("right"):
 		total_rotation += rotation_speed
-	
-	
-	
+
+
 	rotation_degrees += total_rotation
