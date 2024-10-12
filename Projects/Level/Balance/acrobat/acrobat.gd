38acfba846a12462705d72e37b4cd3c109dd29e7
Julieta2200 <julieta@megista.io>
2024-10-12 20:47:35 +0400

    gravitation speed

--- Projects/Level/Balance/acrobat/acrobat.gd
+++ Projects/Level/Balance/acrobat/acrobat.gd
@@ -3,37 +3,32 @@ class_name Acrobat extends Node2D
 @export var rotation_speed: float
 @export var move_speed: int
 @export var move_multiplier: int
-@export var gravitation_speed: float
-var distance_traveled = 0
+@export var initial_gravitation_speed: float
+var gravitation_speed: float
+var distance_traveled: int = 0
 
 func _ready():
-	pass # Replace with function body.
+	gravitation_speed = initial_gravitation_speed
 
 
 func _physics_process(delta):
 	var total_rotation: float
-
-	if rotation_degrees >= 0 && rotation_degrees < 90:
-		if rotation_degrees < 30:
-			total_rotation += gravitation_speed
-		else:
-			total_rotation += 2 * gravitation_speed
-	elif rotation_degrees < 0 && rotation_degrees > -90:
-		if rotation_degrees > -30:
-			total_rotation -= gravitation_speed
-		else:
-			total_rotation -= 2 * gravitation_speed
+	if rotation_degrees > 0:
+		gravitation_speed += rotation_degrees / 100
+		total_rotation += gravitation_speed 
 	else:
-		total_rotation -= 0
-
+		gravitation_speed -= rotation_degrees / 100
+		total_rotation -= gravitation_speed 
+		
 	if Input.is_action_pressed("A"):
 		distance_traveled += 10
 		total_rotation *= move_multiplier
 
 	if Input.is_action_pressed("left"):
-		total_rotation -= rotation_speed
+		total_rotation -= rotation_speed + gravitation_speed
+		gravitation_speed = initial_gravitation_speed
 	elif Input.is_action_pressed("right"):
-		total_rotation += rotation_speed
-
+		total_rotation += rotation_speed + gravitation_speed 
+		gravitation_speed = initial_gravitation_speed
 
 	rotation_degrees += total_rotation
