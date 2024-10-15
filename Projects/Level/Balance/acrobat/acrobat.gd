00c9b66baf666d572c9a10f80fc7f20132aa9419
Julieta2200 <julieta@megista.io>
2024-10-15 17:11:29 +0400

    total rotation

--- Projects/Level/Balance/acrobat/acrobat.gd
+++ Projects/Level/Balance/acrobat/acrobat.gd
@@ -3,32 +3,27 @@ class_name Acrobat extends Node2D
 @export var rotation_speed: float
 @export var move_speed: int
 @export var move_multiplier: int
-@export var initial_gravitation_speed: float
-var gravitation_speed: float
+@export var gravitation_speed: float
 var distance_traveled: int = 0
 
 func _ready():
-	gravitation_speed = initial_gravitation_speed
+	pass
 
 
 func _physics_process(delta):
 	var total_rotation: float
 	if rotation_degrees > 0:
-		gravitation_speed += rotation_degrees / 100
-		total_rotation += gravitation_speed 
+		total_rotation = (rotation_degrees/100) + gravitation_speed 
 	else:
-		gravitation_speed -= rotation_degrees / 100
-		total_rotation -= gravitation_speed 
-		
+		total_rotation = (rotation_degrees/100) - gravitation_speed 
+	
 	if Input.is_action_pressed("A"):
 		distance_traveled += 10
 		total_rotation *= move_multiplier
 
 	if Input.is_action_pressed("left"):
-		total_rotation -= rotation_speed + gravitation_speed
-		gravitation_speed = initial_gravitation_speed
+		total_rotation -= rotation_speed 
 	elif Input.is_action_pressed("right"):
-		total_rotation += rotation_speed + gravitation_speed 
-		gravitation_speed = initial_gravitation_speed
+		total_rotation += rotation_speed
 
 	rotation_degrees += total_rotation
