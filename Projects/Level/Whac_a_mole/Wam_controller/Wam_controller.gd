e5a9f5b8ead1e13dbe296cd2398af746bf645331
Julieta2200 <julieta@megista.io>
2024-11-25 00:29:10 +0400

    player controller

--- Projects/Level/Whac_a_mole/Wam_controller/Wam_controller.gd
+++ Projects/Level/Whac_a_mole/Wam_controller/Wam_controller.gd
@@ -6,10 +6,11 @@ var player_controller : Dictionary = {"left" : false, "right" : false, "up" : fa
 var computer_controller : Dictionary = {"left" : false, "right" : false, "up" : false, "down" : false, "hit" : false}
 
 func input_action_pressed(controller):
-	controller["left"] = Input.is_action_pressed("player_"+ player_number+ "_left")
-	controller["right"] = Input.is_action_pressed("player_"+ player_number + "_right")
-	controller["up"] = Input.is_action_pressed("player_"+ player_number + "_up")
-	controller["down"] = Input.is_action_pressed("player_"+ player_number  + "_down")
-	controller["hit"] = Input.is_action_just_pressed("player_"+ player_number + "_hit")
+	if controller == player_controller:
+		controller["left"] = Input.is_action_pressed("player_"+ player_number+ "_left")
+		controller["right"] = Input.is_action_pressed("player_"+ player_number + "_right")
+		controller["up"] = Input.is_action_pressed("player_"+ player_number + "_up")
+		controller["down"] = Input.is_action_pressed("player_"+ player_number  + "_down")
+		controller["hit"] = Input.is_action_just_pressed("player_"+ player_number + "_hit")
 
 	
